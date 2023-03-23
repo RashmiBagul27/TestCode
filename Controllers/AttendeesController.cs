cfd38074c523a84691fbd61210071a9edd2ead9f
rashmi bagul <rash.bagul@gmail.com>
2023-03-23 19:20:05 +0000

    code for expo attendee app

---
+++ Controllers/AttendeesController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ExpoAttendeeApp;
+
+namespace ExpoAttendeeApp.Controllers
+{
+    public class AttendeesController : Controller
+    {
+        private AttendeeExpoEntities db = new AttendeeExpoEntities();
+
+        // GET: Attendees
+        public ActionResult Index()
+        {
+            return View(db.Attendees.ToList());
+        }
+
+        // GET: Attendees/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendee attendee = db.Attendees.Find(id);
+            if (attendee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attendee);
+        }
+
+        // GET: Attendees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Attendees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FirstName,LastName,Gender,Mobile,PostCode,City,Email")] Attendee attendee)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var isEmailAlreadyExists = db.Attendees.Any(x => x.Email == attendee.Email);
+                if (isEmailAlreadyExists)
+                {
+                    ModelState.AddModelError("Email", "User with this email already exists");
+                    return View(attendee);
+                }
+
+                var isMobileAlreadyExists = db.Attendees.Any(x => x.Mobile == attendee.Mobile);
+                if (isMobileAlreadyExists)
+                {
+                    ModelState.AddModelError("Mobile", "User with this mobile already exists");
+                    return View(attendee);
+                }
+                db.Attendees.Add(attendee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(attendee);
+        }
+
+        // GET: Attendees/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendee attendee = db.Attendees.Find(id);
+            if (attendee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attendee);
+        }
+
+        // POST: Attendees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Gender,Mobile,PostCode,City,Email")] Attendee attendee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(attendee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(attendee);
+        }
+
+        // GET: Attendees/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendee attendee = db.Attendees.Find(id);
+            if (attendee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attendee);
+        }
+
+        // POST: Attendees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Attendee attendee = db.Attendees.Find(id);
+            db.Attendees.Remove(attendee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
